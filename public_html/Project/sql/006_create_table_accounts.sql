890dd546a421a7a091bec90b1f0adc03a80aca52
GamerJunction <rj.ortiz@optonline.net>
2022-04-07 10:55:05 -0400

    Created Accounts Table

---
+++ public_html/Project/sql/006_create_table_accounts.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS  `Accounts`
+(
+    `id`             int auto_increment not null,
+    `account_number` int(12),
+    `user_id`        int,
+    `balance`        int default 0,
+    `account_type`   varchar(255),
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
+    UNIQUE(`account_number`)
+)
