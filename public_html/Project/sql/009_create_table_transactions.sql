19aba85004428a970f6aa9094fd7339ff4fdb7cc
GamerJunction <rj.ortiz@optonline.net>
2022-04-07 19:40:45 -0400

    Transactions Table Created

---
+++ public_html/Project/sql/009_create_table_transactions.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS  `Transactions`
+(
+    `id`                int auto_increment not null,
+    `account_src`       int(12),
+    `account_dest`      int(12),
+    `balance_change`    int default 0,
+    `transaction_type`  varchar(255),
+    `memo`              varchar(255),
+    `expected_total`    int,
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+)
