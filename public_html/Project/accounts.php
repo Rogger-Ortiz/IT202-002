34c19c980ca952bd72533bfba14bb1fea7cc60d7
GamerJunction <rj.ortiz@optonline.net>
2022-04-30 11:12:00 -0400

    Accounts Display Fix

--- public_html/Project/accounts.php
+++ public_html/Project/accounts.php
@@ -14,7 +14,7 @@ if (is_logged_in(true)) {
  $db = getDB();
  $results = [];
  $uid = get_user_id();
- $stmt = $db->prepare("SELECT id, account_number, account_type, modified, balance FROM Accounts WHERE user_id = $uid ORDER BY modified desc LIMIT 1");
+ $stmt = $db->prepare("SELECT id, account_number, account_type, modified, balance FROM Accounts WHERE user_id = $uid ORDER BY modified desc LIMIT 10");
  $stmt->execute();
  $l = $stmt->fetchAll(PDO::FETCH_ASSOC);
  if ($l) {
