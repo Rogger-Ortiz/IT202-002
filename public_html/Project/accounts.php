9e32c44165855186c71710b6570ecbfe0ffb091c
GamerJunction <rj.ortiz@optonline.net>
2022-05-07 17:43:02 -0400

    test

--- public_html/Project/accounts.php
+++ public_html/Project/accounts.php
@@ -8,6 +8,7 @@ if (is_logged_in(true)) {
     //comment this out if you don't want to see the session variables
     error_log("Session data: " . var_export($_SESSION, true));
 }
+$var = "hi";
 ?>
 
 <?php
