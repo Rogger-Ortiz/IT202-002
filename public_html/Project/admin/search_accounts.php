06b9b1e795bb8c276d2166858014cf3e0c3facc8
GamerJunction <rj.ortiz@optonline.net>
2022-05-07 19:34:07 -0400

    Content 3

--- public_html/Project/admin/search_accounts.php
+++ public_html/Project/admin/search_accounts.php
@@ -1,80 +1,87 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
-?>
-<h1>Accounts</h1>
+//note we need to go up 1 more directory
+require(__DIR__ . "/../../../partials/nav.php");
 
-<?php
-if (is_logged_in(true)) {
-    //comment this out if you don't want to see the session variables
-    error_log("Session data: " . var_export($_SESSION, true));
+if (!has_role("Admin")) {
+    flash("You don't have permission to view this page", "warning");
+    die(header("Location: " . get_url("home.php")));
 }
 ?>
+<h1>List Users</h1>
+
+<h3>Filter:</h3>
+<form method="POST">
+    <label for="fname">First Name:</label>
+    <input type="text" id="fname" name="fname"><br>
+
+    <label for="lname">Last Name:</label>
+    <input type="text" id="lname" name="lname"><br>
+
+    <input type="submit" name="submit" value="Filter" />
+</form>
 
 <?php
- $db = getDB();
- $results = [];
- $uid = get_user_id();
- $stmt = $db->prepare("SELECT id, account_number, account_type, modified, balance FROM Accounts WHERE user_id = $uid AND is_active = 1 ORDER BY modified desc LIMIT 10");
- $stmt->execute();
- $l = $stmt->fetchAll(PDO::FETCH_ASSOC);
- if ($l) {
-     $results = $l;
- }
-?>
+    $db = getDB();
+    $users = [];
+    $query = "SELECT id, email, created, username, first_name, last_name, public FROM Users WHERE id>0";
 
+    if(isset($_POST['submit'])){
+        if(isset($_POST['fname'])){
+            $fname = $_POST['fname'];
+            if(ctype_alpha($fname)){
+                $query .= " AND first_name = '$fname'";
+            }
+        }
+        if(isset($_POST['lname'])){
+            $lname = $_POST['lname'];
+            if(ctype_alpha($lname)){
+                $query .= " AND last_name = '$lname'";
+            }
+        }
+    }
+
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $l = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($l) {
+        $users = $l;
+    }
+?>
 <table>
     <thead>
-        <th>Account Number</th>
-        <th>Account Type</th>
-        <th>Modified</th>
-        <th>Balance</th>
-        <th>APY</th>
+        <th>ID</th>
+        <th>Email</th>
+        <th>Created</th>
+        <th>Username</th>
+        <th>First Name</th>
+        <th>Last Name</th>
     </thead>
     <tbody>
-        <?php if (empty($results)) : ?>
+        <?php if (empty($users)) : ?>
             <tr>
-                <td colspan="100%">No Accounts</td>
+                <td colspan="100%">No users</td>
             </tr>
         <?php else : ?>
-            <?php foreach ($results as $item) : ?>
+            <?php foreach ($users as $user) : ?>
                 <tr>
-                    <td><a href="<?php echo get_url('details.php'); ?>?account=<?php se($item, "id");?>&page=1"><?php se($item, "account_number"); ?></a></td>
-                    <td><?php se($item, "account_type"); ?></td>
-                    <td><?php se($item, "modified"); ?></td>
-                    <td><?php se($item, "balance"); ?></td>
-                    <td>
-                        <?php
-                            $acc = $item["account_type"];
-                            $sys = [];
-                            $stmt = $db->prepare("SELECT * FROM Sys");
-                            $stmt->execute();
-                            $l = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                            if ($l) {
-                                $sys = $l;
-                            }
-
-                            if($acc == "Loan" || $acc == "Savings"){
-                                if($acc == "Loan"){
-                                    $apy = $sys[0]["loan_apy"];
-                                    $apy*=100;
-                                    echo "$apy" . "%";
-                                }
-                                if($acc == "Savings"){
-                                    $apy = $sys[0]["savings_apy"];
-                                    $apy*=100;
-                                    echo "$apy" . "%";
-                                }
-                            }else{
-                                echo "-";
-                            }
-                        ?>
-                    </td>
+                    <td><?php se($user, "id"); ?></td>
+                    <td><?php
+                        if($user['public'] == 1){
+                            se($user, 'email');
+                        }else{
+                            echo "-";
+                        }
+                    ?></td>
+                    <td><?php se($user, "created"); ?></td>
+                    <td><?php se($user, "username")?></td>
+                    <td><?php se($user, "first_name")?></td>
+                    <td><?php se($user, "last_name")?></td>
                 </tr>
             <?php endforeach; ?>
         <?php endif; ?>
     </tbody>
 </table>
-
 <?php
-require(__DIR__ . "/../../partials/flash.php");
+//note we need to go up 1 more directory
+require_once(__DIR__ . "/../../../partials/flash.php");
 ?>
