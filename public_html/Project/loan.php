17cab02de23baec73ce377e4bb2ab677000b07ee
GamerJunction <rj.ortiz@optonline.net>
2022-05-07 00:11:40 -0400

    nine of many

--- public_html/Project/loan.php
+++ public_html/Project/loan.php
@@ -184,15 +184,15 @@ if(isset($_POST["loan"]) && !$hasError){
     $uAccID = (int)$uAccID;
 
 
-
+    
     # Minus balance for world
-    $stmt2 = $db->prepare("INSERT INTO Transactions(account_src, account_dest, balance_change, transaction_type, memo, expected_total) VALUES($worldacc, $accnum, ($bal*-1), 'Deposit', 'Loan Taken', $wbal )");
+    $stmt2 = $db->prepare("INSERT INTO Transactions(account_src, account_dest, balance_change, transaction_type, memo, expected_total) VALUES($worldacc, $uAccID, ($bal*-1), 'Deposit', 'Loan Taken', $wbal)");
 
     # Minus balance for loan acc
-    $stmt3 = $db->prepare("INSERT INTO Transactions(account_src, account_dest, balance_change, transaction_type, memo, expected_total) VALUES($accnum, $worldacc, $bal, 'Deposit', 'Loan Taken', $bal)");
+    $stmt3 = $db->prepare("INSERT INTO Transactions(account_src, account_dest, balance_change, transaction_type, memo, expected_total) VALUES($uAccID, $worldacc, $bal, 'Deposit', 'Loan Taken', $bal)");
 
     # Plus balace for destination account
-    $stmt4 = $db->prepare("INSERT INTO Transactions(account_src. account_dest, balance_change, transaction_type, memo, expected_total) VALUES($accnum, $destID, $bal, 'Deposit', 'Loan Deposited', $newbal)");
+    $stmt4 = $db->prepare("INSERT INTO Transactions(account_src. account_dest, balance_change, transaction_type, memo, expected_total) VALUES($uAccID, $destID, ($bal*-1), 'Deposit', 'Loan Deposited', $newbal)");
 
     try {
         $stmt2->execute();
